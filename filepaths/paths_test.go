82979b20d350fa087cbd60f08d57ff647fa1b94d
Simão Gomes Viana <simao.gomes@toowoxx.de>
2021-11-05 14:27:17 +0100

    Create paths_test.go

---
+++ filepaths/paths_test.go
@@ -0,0 +1,43 @@
+package filepaths
+
+import "testing"
+
+func TestComponents(t *testing.T) {
+	c := Components("/")
+	if len(c) != 1 {
+		t.Fatal("expected one empty string, instead got", c)
+	}
+	if c[0] != "" {
+		t.Fatal("expected first string to be empty, instead got", c[0])
+	}
+
+	c = Components("/1/2/3.txt")
+	if len(c) != 4 {
+		t.Fatal("expected 4 strings, instead got", c)
+	}
+	if c[0] != "" {
+		t.Fatal("expected first string to be empty, instead got", c[0])
+	}
+	if c[1] != "1" {
+		t.Fatal("expected first string to be '1', instead got", c[1])
+	}
+	if c[2] != "2" {
+		t.Fatal("expected second string to be '2', instead got", c[2])
+	}
+	if c[3] != "3.txt" {
+		t.Fatal("expected third string to be '3.txt', instead got", c[3])
+	}
+
+	c = Components("abc.txt")
+	if len(c) != 1 {
+		t.Fatal("expected one string, instead got", c)
+	}
+	if c[0] != "abc.txt" {
+		t.Fatal("expected first string to be 'abc.txt', instead got", c[0])
+	}
+
+	c = Components("")
+	if len(c) != 0 {
+		t.Fatal("expected empty slice, instead got", c)
+	}
+}
